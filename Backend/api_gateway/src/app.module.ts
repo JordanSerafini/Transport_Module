10e5c0c9e7fa15b4097295fe576275a7b5c10e45
JordanSerafini <jordanserafini.74@gmail.com>
2024-12-19 13:22:10 +0100

    :construction: building backend microservice with nestJS

--- Backend/api_gateway/src/app.module.ts
+++ Backend/api_gateway/src/app.module.ts
@@ -1,10 +1,64 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { AuthController } from './controllers/auth/auth.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_secret',
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '100d' },
+    }),
+    ClientsModule.register([
+      {
+        name: 'auth_service',
+        transport: Transport.TCP,
+        options: { port: 3001, host: 'auth_service' },
+      },
+      {
+        name: 'companies_service',
+        transport: Transport.TCP,
+        options: { port: 3002, host: 'user_service' },
+      },
+      {
+        name: 'customers_service',
+        transport: Transport.TCP,
+        options: { port: 3002, host: 'customers_service' },
+      },
+      {
+        name: 'invoices_service',
+        transport: Transport.TCP,
+        options: { port: 3006, host: 'invoices_service' },
+      },
+      {
+        name: 'kpi_service',
+        transport: Transport.TCP,
+        options: { port: 3009, host: 'kpi_service' },
+      },
+      {
+        name: 'notifications_service',
+        transport: Transport.TCP,
+        options: { port: 3008, host: 'notifications_service' },
+      },
+      {
+        name: 'orders_service',
+        transport: Transport.TCP,
+        options: { port: 3004, host: 'orders_service' },
+      },
+      {
+        name: 'products_service',
+        transport: Transport.TCP,
+        options: { port: 3003, host: 'products_service' },
+      },
+      {
+        name: 'shipments_service',
+        transport: Transport.TCP,
+        options: { port: 3006, host: 'shipments_service' },
+      },
+    ]),
+  ],
+  providers: [JwtAuthGuard],
+  controllers: [AuthController],
 })
 export class AppModule {}
--- Backend/authentification_service/src/app.module.ts
+++ Backend/authentification_service/src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Logger, Module } from '@nestjs/common';
+import { AuthService } from './app.service';
+import { AuthController } from './app.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { CustomLogger } from './logging/custom-logger.service';
+import { PgConnectionModule } from 'pool_package';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JwtModule.register({
+      secret: 'mdp',
+      signOptions: { expiresIn: '100d' },
+    }),
+    PgConnectionModule,
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    { provide: Logger, useClass: CustomLogger },
+  ],
 })
 export class AppModule {}
